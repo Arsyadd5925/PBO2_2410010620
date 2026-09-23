a655415bb4e7d09766c58c3deec4d98861ca2e85
Muhammad Arsyad <wpcell86@gmail.com>
2026-09-23 19:48:18 +0800

    first commit

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author MSI SERIES
+ */
+public class KartuMahasiswa {
+    public static void main(String[] args) {
+
+        // Data mahasiswa
+        String nama = "MUHAMMAD ARSYAD";
+        String npm = "2410010620";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin membuat aplikasi perpustakaan.";
+
+        // Menampilkan kartu mahasiswa
+        System.out.println("================================");
+        System.out.println("      KARTU MAHASISWA PBO 2");
+        System.out.println("================================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("================================");
+    }
+
+    
+}
